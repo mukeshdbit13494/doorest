08ce616bce4db4ee0828ea9dfdcd4ab521922df7
Mukesh Singh <48683217+mukeshdbit13494@users.noreply.github.com>
2021-01-26 21:50:42 +0530

    Update addItemTab.js
    change only names

--- components/admin/addItemTab.js
+++ components/admin/addItemTab.js
@@ -50,7 +50,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function AddItems() {
+export default function AddItemTab() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
